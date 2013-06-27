1bcce4aaa9a5017187b8d50f456a9bcdfba92c30
Wesley Verne <wverne@princeton.edu>
2013-06-26 22:49:26 -0400

    more model changes and update to map_test

--- foodtrucks/models.py
+++ foodtrucks/models.py
@@ -4,18 +4,21 @@ class FoodTruck(models.Model):
     name           = models.CharField(max_length=100)    
     website        = models.URLField()
     twitter        = models.URLField()
-    website        = models.URLField()
+    yelp           = models.URLField()
     city           = models.CharField(max_length=100)
     state_province = models.CharField(max_length=100, 
                                       verbose_name="state/province")
-    image_name     = models.CharField(max_length=100)
 
+    image_name     = models.CharField(max_length=100)
+    description    = models.TextField()
+    menu           = models.TextField()
+
+    last_checkin   = models.DateTimeField()
+    latitude       = models.FloatField()
+    longitude      = models.FloatField()
 
     def __unicode__(self):
         return self.name
 
     class Meta:
         ordering = ['name', 'city']
-
-
-# Create your models here.
