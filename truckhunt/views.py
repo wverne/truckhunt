59f77573cebf60595c92b6531a777a3bb7625009
Wesley Verne <wverne@princeton.edu>
2013-06-27 11:33:51 -0400

    yet more truck pages

--- truckhunt/views.py
+++ truckhunt/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, render_to_response
 from django.template.loader import get_template
 from django.template import Context
-from django.http import HttpResponse
+from django.http import *
 from gmapi import maps
 from truckhunt.forms import *
 from foodtrucks.models import *
@@ -10,9 +10,18 @@ def homepage(request):
     return render(request, 'homepage.html', 
                   {'trucks_list': FoodTruck.objects.all()})
 
-def trucks_page(request):
+def trucks_page(request, truckname=""):
+    if (truckname==""):
+        return render(request, 'trucks.html',
+                      {'trucks_list': FoodTruck.objects.all()})
+
+    # find appropriate truck
+    truck_object = FoodTruck.objects.filter(name=truckname)
+    if not truck_object:
+        raise Http404
+
     return render(request, 'trucks.html',
-                  {'trucks_list': FoodTruck.objects.all()})
+                  {'trucks_list': truck_object})
 
 def types_page(request):
     return render(request, 'types.html', {})
