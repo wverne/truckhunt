c34924efe931b3db8edea41ba9747b6bc4e68aef
Wesley Verne <wverne@princeton.edu>
2013-06-26 23:13:07 -0400

    added foodtruck to installed apps

--- truckhunt/settings.py
+++ truckhunt/settings.py
@@ -127,6 +127,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    'foodtruck',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
