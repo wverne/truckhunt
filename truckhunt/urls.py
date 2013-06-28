2ca34c7a15fb5c5621e0c30f99de09e75a0314f9
Sam Roberts <samlroberts@gmail.com>
2013-06-28 00:12:44 -0400

    merge with wverne changes

--- truckhunt/urls.py
+++ truckhunt/urls.py
@@ -8,17 +8,23 @@ admin.autodiscover()
 urlpatterns = patterns('truckhunt.views',
                        (r'^$',       'homepage'),
                        (r'^trucks/$', 'trucks_page'),
-                       (r'^types/$', 'types_page'),
+                       (r'^trucks/(.+)/$', 'trucks_page'),
                        (r'^featured/$', 'featured_page'),
+                       (r'^about/$', 'about_page'),
                        (r'^map_test/$', 'map_test_page'),
-                       )
+                       (r'^login/$', 'login_view'),
+                       (r'^logout/$', 'logout_view'),
+                       (r'^disabled_account/$', 'disabled_page'),
+                       (r'^invalid_login/$', 'invalid_login_page'),
+                       (r'^user/(.+)/$', 'user_page'),
+                      )
 
 urlpatterns += patterns('',
                         (r'^admin/', include(admin.site.urls)),
-                        (r'^static/(.*)$', 
+                        (r'^static/(.+)$', 
                          'django.views.static.serve', 
                          {'document_root': settings.STATIC_ROOT}),
 
         # Uncomment the admin/doc line below to enable admin documentation
         # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-                        )
+                       )
--- truckhunt/views.py
+++ truckhunt/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, redirect
 from django.template.loader import get_template
 from django.template import Context
-from django.http import HttpResponse
+from django.http import *
+from django.contrib.auth import authenticate, login, logout
+
 from gmapi import maps
+
 from truckhunt.forms import *
 from foodtrucks.models import *
 
@@ -10,14 +13,61 @@ def homepage(request):
     return render(request, 'homepage.html', 
                   {'trucks_list': FoodTruck.objects.all()})
 
-def trucks_page(request):
-    return render(request, 'trucks.html', {})
+def trucks_page(request, truckname=""):
+    if (truckname==""):
+        return render(request, 'trucks.html',
+                      {'trucks_list': FoodTruck.objects.all()})
 
-def types_page(request):
-    return render(request, 'types.html', {})
+    # find appropriate truck
+    truck_object = FoodTruck.objects.filter(name=truckname)
+    if not truck_object:
+        raise Http404
+
+    return render(request, 'truck_page.html',
+                  {'trucks_list': FoodTruck.objects.all(),
+                   'this_truck':  truck_object[0]})
 
 def featured_page(request):
     return render(request, 'featured.html', {})
 
+def about_page(request):
+    return render(request, 'about.html', {})
+
 def map_test_page(request):
-    return render(request, 'map_test.html', {})
+    return render(request, 'map_test.html',
+                  {'trucks_list': FoodTruck.objects.all()})
+
+def login_view(request):
+    if not 'username' in request.POST:
+        return redirect('/invalid_login/')
+    if not 'password' in request.POST:
+        return redirect('/invalid_login/')
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return redirect('/')
+        else:
+            return redirect('/disabled_account/')
+    return redirect('/invalid_login/')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+def disabled_page(request):
+    return render(request, 'disabled_account.html', {})
+
+def invalid_login_page(request):
+    return render(request, 'invalid_login.html', {})
+
+def user_page(request, inusername):
+    # find appropriate user
+    user_object = User.objects.filter(username=inusername)
+    if not user_object:
+        raise Http404
+
+    return render(request, 'user_page.html',
+                  {'this_user':  user_object[0]})
