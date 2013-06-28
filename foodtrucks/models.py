15f30c5124049f75ba9fe405e352c55271f74594
Sam Roberts <samlroberts@gmail.com>
2013-06-28 00:13:11 -0400

    rest of merge changes

--- foodtrucks/models.py
+++ foodtrucks/models.py
@@ -1,21 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class FoodTruck(models.Model):
     name           = models.CharField(max_length=100)    
-    website        = models.URLField(null=True)
-    twitter        = models.URLField(null=True)
-    yelp           = models.URLField(null=True)
-    city           = models.CharField(max_length=100)
-    state_province = models.CharField(max_length=100, 
+    website        = models.URLField(null=True, blank=True)
+    twitter        = models.URLField(null=True, blank=True)
+    yelp           = models.URLField(null=True, blank=True)
+    city           = models.CharField(max_length=100, blank=True)
+    state_province = models.CharField(max_length=100, blank=True,
                                       verbose_name="state/province")
 
-    image_name     = models.CharField(max_length=100, null=True)
-    description    = models.TextField(null=True)
-    menu           = models.TextField(null=True)
+    image_name     = models.CharField(max_length=100, null=True, blank=True)
+    description    = models.TextField(blank=True)
+    menu           = models.TextField(blank=True)
 
-    last_checkin   = models.DateTimeField(null=True)
-    latitude       = models.FloatField(null=True)
-    longitude      = models.FloatField(null=True)
+    last_checkin   = models.DateTimeField(null=True, blank=True)
+    latitude       = models.FloatField(null=True, blank=True)
+    longitude      = models.FloatField(null=True, blank=True)
+
+    owner          = models.ForeignKey(User)
 
     def __unicode__(self):
         return self.name
